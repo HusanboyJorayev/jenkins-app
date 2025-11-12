1c543640cc5238d7a8c68e586fad850b9c7d7b16
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2025-11-12 09:14:05 +0500

    first commit

---
+++ src/main/java/org/example/jenkins/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package org.example.jenkins.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Demo Jenkins APIs", // Optional: Qo'shimcha ma'lumot berish uchun
+                version = "2.0.0", // OpenAPI versiyasi noto'g'ri emas, faqat ma'lumot sifatida
+                description = "Demo Jenkins API Documentation"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8765/jenkins", description = "Local Server"),
+                @Server(url = "https://agrosphere.uz/jenkins", description = "Prod Server")
+        },
+        security = @SecurityRequirement(name = "bearerAuth") // Apply globally
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT", // Optional: Defines the format (JWT)
+        in = SecuritySchemeIn.HEADER,
+        paramName = "Authorization"
+)
+public class SwaggerConfig {
+
+}
